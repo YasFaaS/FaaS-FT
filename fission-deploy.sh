e7b1474882d77927a3dea7d0a378d7a62df8e932
YasFaaS <104907860+YasFaaS@users.noreply.github.com>
2022-05-05 11:25:16 +0200

    Update and rename fission-update.sh to fission-deploy.sh

---
+++ fission-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+
+# we have already build the container image for Fission vanilla, AS and RR 
+
+#install fission with the new image 
+
+#install of Fission vanilla
+helm install --set "image=cloudfaas/fission-bundle,imageTag=1.10.0,pullPolicy=IfNotPresent,analytics=false,serviceType=NodePort,routerServiceType=NodePort" charts/fission-all
+#install of Fission Request Replication
+helm install --set "image=cloudfaas/fission-bundle,imageTag=1.10.1,pullPolicy=IfNotPresent,analytics=false,serviceType=NodePort,routerServiceType=NodePort" charts/fission-all
+#instal of Fission Active-Standby
+helm install --set "image=cloudfaas/fission-bundle,imageTag=1.10.2,pullPolicy=IfNotPresent,analytics=false,serviceType=NodePort,routerServiceType=NodePort" charts/fission-all
+
+
+
+
+
