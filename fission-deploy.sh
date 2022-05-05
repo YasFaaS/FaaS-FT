1edaa5ca1aceca55fcf0dfb0a74f01973536ad0a
YasFaaS <104907860+YasFaaS@users.noreply.github.com>
2022-05-05 13:31:33 +0200

    Update fission-deploy.sh

--- fission-deploy.sh
+++ fission-deploy.sh
@@ -13,6 +13,11 @@ helm install --set "image=cloudfaas/fission-bundle,imageTag=1.10.1,pullPolicy=If
 #instal of Fission Active-Standby
 helm install --set "image=cloudfaas/fission-bundle,imageTag=1.10.2,pullPolicy=IfNotPresent,analytics=false,serviceType=NodePort,routerServiceType=NodePort" charts/fission-all
 
+echo "Installing the fission CLI..."
+ 
+curl -Lo fission https://github.com/fission/fission/releases/download/1.10.0/fission-cli-linux && chmod +x fission && sudo mv fission /usr/local/bin/
+
+
 
 
 
